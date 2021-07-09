0b42ec8ad9513aa118c34d09657f7462e7414ecc
Lucas Mezencio <lucasmezencio@outlook.com>
2021-07-09 13:04:40 -0300

    updated to talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,11 +4,15 @@ class Pessoa {
     var nome: String = "Lucas"
     var cpf: String = "111.222.333-44"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val lucas = Pessoa()
 
-    println(lucas.nome)
-    println(lucas.cpf)
+    println(lucas)
+    println(lucas.pessoaInfo())
 }
