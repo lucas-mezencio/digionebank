47acf456b17181943cbe01af7dc99aaf993cf0b0
Lucas Mezencio <lucasmezencio@outlook.com>
2021-07-09 12:54:42 -0300

    updated to apply the private modifier set function of cpf property

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,10 +3,12 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Lucas"
     var cpf: String = "111.222.333-44"
+    private set
 }
 
 fun main() {
     val lucas = Pessoa()
 
     println(lucas.nome)
+    println(lucas.cpf)
 }
