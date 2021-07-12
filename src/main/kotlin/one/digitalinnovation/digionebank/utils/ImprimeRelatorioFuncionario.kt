7dfab8ffae95f85abb8453e6b87387f618b2c50c
Lucas Mezencio <lucasmezencio@outlook.com>
2021-07-12 13:47:37 -0300

    Created util to print employee report

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/utils/ImprimeRelatorioFuncionario.kt
@@ -0,0 +1,9 @@
+package one.digitalinnovation.digionebank.utils
+
+import one.digitalinnovation.digionebank.Funcionario
+
+class ImprimeRelatorioFuncionario {
+    fun imprime(funcionario: Funcionario) {
+        println(funcionario.toString())
+    }
+}
