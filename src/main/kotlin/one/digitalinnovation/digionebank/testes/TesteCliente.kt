c9099bcaa2c5f0b5957b5550377211b4a7e48fdf
Lucas Mezencio <lucasmezencio@outlook.com>
2021-07-12 14:49:32 -0300

    finished project on course scope

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteCliente.kt
@@ -0,0 +1,16 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Cliente
+import one.digitalinnovation.digionebank.ClienteTipo
+
+fun main() {
+    val jose = Cliente(
+        nome = "José da Silva",
+        cpf = "123.456.789-00",
+        clienteTipo = ClienteTipo.PF,
+        senha = "123456"
+    )
+
+    println(jose)
+    TesteAutenticacao().autentica(jose)
+}
--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -4,6 +4,8 @@ import one.digitalinnovation.digionebank.Gerente
 import one.digitalinnovation.digionebank.utils.ImprimeRelatorioFuncionario
 
 fun main() {
-    val manager = Gerente("John Doe", "123.456.789-10", 5000.0)
+    val manager = Gerente("John Doe", "123.456.789-10", 5000.0, "senha123")
     ImprimeRelatorioFuncionario.imprime(manager)
+
+    TesteAutenticacao().autentica(manager)
 }
